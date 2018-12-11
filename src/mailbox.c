6586b4fa75d3fe190133364dcb29882dbf72154e
Martchus <martchus@gmx.net>
2018-12-11 18:31:37 +0000

    Prevent "undefined reference to `makedev'"

--- src/mailbox.c
+++ src/mailbox.c
@@ -35,6 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <sys/sysmacros.h>
 
 #include "mailbox.h"
 
